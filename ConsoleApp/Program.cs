ac9747f1552bd437bb2f3ebc0afecd6b685a0a66
Владислав Сологуб <solohub.v@donnu.edu.ua>
2021-02-28 15:47:41 +0200

    Змінено з IDE

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Underworld!"); //comment browser
+            Console.WriteLine("Hello Underworld!"); //comment IDE
         }
     }
 }
