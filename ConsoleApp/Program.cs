7317b3122874119664e9353af7620dc9205ef155
Владислав Сологуб <solohub.v@donnu.edu.ua>
2021-02-28 15:44:19 +0200

    Фіксування проблеми

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Underworld!"); //comment
+<<<<<<< HEAD
+            Console.WriteLine("Hello Underworld!"); //comment IDE
+=======
+            Console.WriteLine("Hello Underworld!"); //comment browser
+>>>>>>> ab0cc821808e0d4f27b225612381aeb9e068df50
         }
     }
 }
